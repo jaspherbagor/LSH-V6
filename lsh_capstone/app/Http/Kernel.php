4f231d1885a6c0f40fb3042621a540dbb5097964
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-06-12 08:35:29 +0800

    registered the customer middleware in the kernel.php

--- lsh_capstone/app/Http/Kernel.php
+++ lsh_capstone/app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'customer' => \App\Http\Middleware\Customer::class
+        'customer' => \App\Http\Middleware\Customer::class,
+        'accommodation' => \App\Http\Middleware\Accommodation::class
     ];
 }
