d2bb8809258d53a3a3ff98f837a72cb047a30144
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-06-13 15:37:32 +0800

    added customer's info in the orders table of the admin panel

--- lsh_capstone/resources/views/admin/orders.blade.php
+++ lsh_capstone/resources/views/admin/orders.blade.php
@@ -14,6 +14,7 @@
                                     <tr>
                                         <th>SL</th>
                                         <th>Booking No</th>
+                                        <th>Customer's Name</th>
                                         <th>Payment Method</th>
                                         <th>Booking Date</th>
                                         <th>Paid Amount</th>
@@ -23,9 +24,13 @@
                                 </thead>
                                 <tbody>
                                     @foreach($orders as $row)
+                                    @php 
+                                    $customer_info = \App\Models\Customer::where('id', $row->customer_id)->first();
+                                    @endphp
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $row->order_no }}</td>
+                                        <td>{{ $customer_info->name }}</td>
                                         <td>{{ $row->payment_method }}</td>
                                         <td>{{ \Carbon\Carbon::createFromFormat('d/m/Y', $row->booking_date)->format('F d, Y') }}</td>
                                         <td>₱{{ number_format($row->paid_amount, 2) }}</td>
