6a39f83b40905330d6c0cb356353cbfd1fadd9e7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-06-24 14:29:46 +0800

    removed the button beside the accommodation invoice heading

--- lsh_capstone/resources/views/accommodation/invoice.blade.php
+++ lsh_capstone/resources/views/accommodation/invoice.blade.php
@@ -2,10 +2,6 @@
 
 @section('heading', 'Booking Invoice')
 
-@section('right_top_button')
-<a href="" class="btn btn-primary"><i class="fa fa-eye"></i> View All</a>
-@endsection
-
 @section('main_content')
 <div class="section-body">
     <div class="invoice">
